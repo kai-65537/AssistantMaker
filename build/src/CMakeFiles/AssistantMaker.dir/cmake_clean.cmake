cf5a54723413c9f6134c258e43ff65c43577b370
Caeruleum Wang <caeruleum@pop-os.localdomain>
2022-07-21 16:53:08 +0800

    fix cmakelists

---
+++ build/src/CMakeFiles/AssistantMaker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AssistantMaker"
+  "AssistantMaker.pdb"
+  "CMakeFiles/AssistantMaker.dir/main.cpp.o"
+  "CMakeFiles/AssistantMaker.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AssistantMaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/AssistantMaker.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AssistantMaker.
+# This may be replaced when dependencies are built.
