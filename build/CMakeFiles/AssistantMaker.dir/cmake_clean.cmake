f08bb97900ddbde212165435f27c8982721d1dee
Caeruleum Wang <caeruleum@pop-os.localdomain>
2022-07-21 16:47:29 +0800

    basic program structure and cmake configuration

---
+++ build/CMakeFiles/AssistantMaker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AssistantMaker"
+  "AssistantMaker.pdb"
+  "CMakeFiles/AssistantMaker.dir/src/main.cpp.o"
+  "CMakeFiles/AssistantMaker.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AssistantMaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/AssistantMaker.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for AssistantMaker.
+# This may be replaced when dependencies are built.
